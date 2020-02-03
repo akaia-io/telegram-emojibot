f2d4375ded5344c7540d7a1b2675fa5743ccdf34
bulgakke <vvp835@yandex.ru>
2020-02-04 02:12:48 +0300

    small fix to chat settings

--- emoji/chat_setting/chat_settings.rb
+++ emoji/chat_setting/chat_settings.rb
@@ -1,10 +1,23 @@
 require 'json'
-require 'telegram/bot'
 
 module Telegram
   module Bot
     module Types
       class Chat
+        def emojimode
+          return @emojimode if @emojimode
+          load_mode_setting
+        end
+
+        def emojimode=(value)
+          @emojimode = value
+          parse_settings_json
+          @@settings.merge!( { self.id => @emojimode } )
+          update_json
+        end
+
+        private 
+
         def load_mode_setting
           parse_settings_json
           return nil unless @@settings[self.id.to_s]
@@ -17,24 +30,8 @@ module Telegram
           @@settings = JSON.parse(@json)
         end
 
-        def emojimode
-          return @emojimode if @emojimode
-          load_mode_setting
-        end
-
         def update_json
           IO.write('./chat_settings.json', @@settings.to_json)
-        end
-
-        def emojimode=(value)
-          @emojimode = value
-          parse_settings_json
-          @@settings.merge!( { self.id => @emojimode } )
-          update_json
-        end
-
-        def id=(value)
-          @id = value
         end
       end
     end
